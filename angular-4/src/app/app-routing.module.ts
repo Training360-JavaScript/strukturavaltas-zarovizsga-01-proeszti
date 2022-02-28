7cbd6969653c647502f917587884a5c41bc19bdd
proeszti <kispothajas@gmail.com>
2022-02-28 16:43:06 +0100

    .

--- angular-4/src/app/app-routing.module.ts
+++ angular-4/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { CarEditorComponent } from 'src/app/page/car-editor/car-editor.component';
+import { CarComponent } from 'src/app/page/car/car.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
@@ -10,6 +12,14 @@ const routes: Routes = [
   {
     path: '**',
     component: HomeComponent,
+  },
+  {
+    path: '/car/edit/44',
+    component: CarEditorComponent,
+  },
+  {
+    path: 'car',
+    component: CarComponent,
   },
 ];
 
